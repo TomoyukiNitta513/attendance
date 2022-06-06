2c9bc754df363db2c507a783c7e3f1d26d2f52b5
vagrant <vagrant@localhost.localdomain>
2022-06-06 10:32:43 +0900

    departmentのhas_many削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :attendance_managements
   has_many :details
-  belongs_to :department
+  # belongs_to :department
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
